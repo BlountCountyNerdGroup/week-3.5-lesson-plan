268fe35cb7184ebd488ae33f1e7f28650b5d8b02
Kaleb Davenport <kaleb@wowyourclient.com>
2018-02-03 10:33:08 -0500

    Add files via upload

--- rpg.js
+++ rpg.js
@@ -1,13 +1,17 @@
-var health = 10;
+var health = 2;
 var money = 100;
 
 function randomNum(min, max) {
-    return Math.random() * (max - min) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 // Challenge: write a function that renders a random random number using the onclick="{function()}" HTML attribute
-// to change the html you will need to use JavaScript's getElementById("{id}") and .innerText = "{some text}"
+// to change the html you will need to use JavaScript's document.getElementById("{id}") and .innerText = "{some text}"
 // Remeber to search google if you have any problems and then ask if you still can't find the answer
+
+function renderRandNum() {
+    document.getElementById("randNumDiv").innerText = randomNum(5, 10);
+}
 
 function renderMoney() {
     document.getElementById("cash").innerHTML = "$" + money;
@@ -19,3 +23,16 @@ function renderHealth() {
 // Render first time through
 renderMoney();
 renderHealth();
+
+
+function getALife() {
+    if (health == 0) {
+        alert("You came back from the dead!");
+        health += 1;
+    }else if (health == 1) {
+        alert("You are well again.");
+        health += 1;
+    }else if (health == 2) {
+        alert("Sorry, you are already in good health");
+    }
+}
